e2208ef1a3d4c703365df199d54885e7c28b939d
Adetomi <ribigbetomi@gmail.com>
2023-08-28 13:47:12 +0100

    twilio

--- index.js
+++ index.js
@@ -16,19 +16,6 @@ app.get("/", (req, res) => {
 
 app.use("/api/twilio", twilioRoute);
 
-// const accountSid = process.env.TWILIO_ACCOUNT_SID;
-// const authToken = process.env.TWILIO_AUTH_TOKEN;
-
-// const client = require("twilio")(accountSid, authToken);
-
-// client.messages
-//   .create({
-//     body: "Adetomi App Registration Trial for Code confirmation",
-//     to: "+447899810854", // Text your number
-//     from: "+447782361338", // From a valid Twilio number
-//   })
-//   .then((message) => console.log(message.sid));
-
 const PORT = process.env.PORT || 1000;
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
